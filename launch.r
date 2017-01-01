6b19d5b453cd7209c76d1ab650f8cb81d0cf1c39
YvesCR <yves.crutain@gmail.com>
2017-01-01 12:28:48 +0000

    moif js

--- launch.r
+++ launch.r
@@ -8,5 +8,9 @@ getwd()
 servr::jekyll(input = "_source", output = "_posts", script = c("Makefile", "build.R"),
             command = "jekyll build --destination ../blog_dev_mm/")
 
+# manual hack for embedocpu: get rid of line "<script src="https://data-laborer.eu/assets/js/main.min.js"></script>" 
+# because it creates a bug in the matrix
 
-
+art <- readLines("../blog_dev_mm/hack/embedocputest/index.html")
+art <- art[!grepl('<script src="https://data-laborer.eu/assets/js/main.min.js"></script>', art)]
+cat(art, file="../blog_dev_mm/hack/embedocputest/index.html", quote = F, fill=T)
